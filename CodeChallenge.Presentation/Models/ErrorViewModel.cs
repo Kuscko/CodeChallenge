fe787c2414142f7bc59287aad8a44e5536904ab5
Patrick Kelly <patrick.kellyse@gmail.com>
2023-02-07 18:55:14 -0500

    Removed the models and main business logic to the CodeChallenge.Core project.
    Calls querys from the Core project to retrieve data for the contact details.
    
    Removed unecessary pages, and added the required nuget packages for entity framework generation.

--- CodeChallenge.Presentation/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CodeChallenge.Presentation.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
